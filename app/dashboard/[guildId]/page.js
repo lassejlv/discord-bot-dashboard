7d57da2e7d2dd64b2bc82a8552cccf948b312d0e
lassev05 <vestergaardlasse2@gmail.com>
2023-08-29 14:26:56 +0200

    Check guild perms and get guild infomation and display

--- app/dashboard/[guildId]/page.js
+++ app/dashboard/[guildId]/page.js
@@ -1,5 +1,22 @@
-import React from "react";
+import { getUser, getGuild } from "@/app/actions";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-export default function page() {
-  return <div>page</div>;
+export default async function page({ params }) {
+  const session = await getServerSession();
+  const user = await getUser(session.user.email);
+  const guild = await getGuild(params.guildId);
+  console.log(getGuild);
+
+  if (!user.guildIds.includes(params.guildId)) {
+    return redirect("/?error=missing_permissions");
+  } else if (!guild) {
+    return redirect("/?error=not_found");
+  }
+
+  return (
+    <>
+      <div>Welcome back {guild?.name}</div>
+    </>
+  );
 }
--- app/page.js
+++ app/page.js
@@ -2,11 +2,14 @@
 import React from "react";
 import { useSession, signIn, signOut } from "next-auth/react";
 import { getUserGuilds } from "./actions";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
   const { data: session } = useSession();
   const [loading, setLoading] = React.useState(false);
   const [guilds, setGuilds] = React.useState([]);
+  const router = useRouter();
 
   React.useEffect(() => {
     if (session) {
@@ -14,11 +17,16 @@ export default function Home() {
       getUserGuilds(session.user.email).then((guilds) => {
         setTimeout(() => {
           setGuilds(guilds);
+          console.log(guilds);
           setLoading(false);
         }, 1000);
       });
     }
   }, [session]);
+
+  const randomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min) + min);
+  };
 
   return (
     <>
@@ -29,23 +37,39 @@ export default function Home() {
             <>Loading...</>
           ) : (
             <>
-              {guilds?.map((guild) => (
-                <div key={guild.id}>
-                  <img
-                    src={`https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}?size=64`}
-                  />
-                  {guild.name} - {guild.inGuild ? "In Guild" : "Not In Guild"}
-                  <a
-                    href={
-                      guild.inGuild
-                        ? `/dashboard/${guild.id}`
-                        : `https://discord.com/api/oauth2/authorize?client_id=1101580219908903053&permissions=8&scope=bot&guild_id=${guild.id}&response_type=code&disable_guild_select=true`
-                    }
-                  >
-                    View
-                  </a>
-                </div>
-              ))}
+              <div
+                className="grid gap-4 m-3"
+                style={{
+                  gridTemplateColumns: "repeat(auto-fit, minmax(100px, 1fr))",
+                }}
+              >
+                {guilds?.map((guild) => (
+                  <>
+                    <article
+                      onClick={() => {
+                        guild?.inGuild
+                          ? router.push(`/dashboard/${guild.id}`)
+                          : router.push(`/dashboard/${guild.id}/invite`);
+                      }}
+                      key={guild.id}
+                      className="overflow-hidden rounded-lg shadow-lg cursor-pointer hover:shadow-2xl transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"
+                    >
+                      <Image
+                        src={
+                          guild?.icon !== null
+                            ? `https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}?size=256`
+                            : `https://cdn.discordapp.com/embed/avatars/${
+                                randomNumber(0, 5) + 1
+                              }.png`
+                        }
+                        alt="guild icon"
+                        width={200}
+                        height={200}
+                      />
+                    </article>
+                  </>
+                ))}
+              </div>
             </>
           )}
         </>
